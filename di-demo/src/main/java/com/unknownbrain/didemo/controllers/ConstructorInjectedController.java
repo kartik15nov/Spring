9409d54de9335a005f3493bfc8d28c7ad84b53b1
Kartik <kartik15nov@gmail.com>
2020-01-21 11:13:08 +0530

    All type of Dependency Injection experiments with Unit test.

---
+++ di-demo/src/main/java/com/unknownbrain/didemo/controllers/ConstructorInjectedController.java
@@ -0,0 +1,19 @@
+package com.unknownbrain.didemo.controllers;
+
+import com.unknownbrain.didemo.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+
+    private GreetingService greetingService;
+
+    //Optional @AutoWired
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
