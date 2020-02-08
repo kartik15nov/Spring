d18e047dfdfc558e8e4446e1f7e38a8df642e910
Kartik <kartik15nov@gmail.com>
2020-02-09 02:03:40 +0530

    Implemented Project Lombok.

--- recipe-app/src/main/java/com/unknownbrain/recipeapp/controller/IndexController.java
+++ recipe-app/src/main/java/com/unknownbrain/recipeapp/controller/IndexController.java
@@ -1,21 +1,22 @@
 package com.unknownbrain.recipeapp.controller;
 
 import com.unknownbrain.recipeapp.services.RecipeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Log4j2
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final RecipeService recipeService;
 
-    public IndexController(RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
-
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
