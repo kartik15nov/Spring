eabbb83d81811dc5d68fed522e0252e3127a833c
Don <kartik15nov@gmail.com>
2020-04-10 21:22:56 +0530

    Added the ControllerAdvice to handle the common exception globally. Added unit tests, moved the handleNumberFormatException from RecipeController to ControllerAdvice.

--- recipe-app/src/main/java/com/unknownbrain/recipeapp/controllers/RecipeController.java
+++ recipe-app/src/main/java/com/unknownbrain/recipeapp/controllers/RecipeController.java
@@ -71,19 +71,4 @@ public class RecipeController {
 
         return modelAndView;
     }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormatException(Exception exception) {
-
-        log.error("Handling Number Format exception");
-        log.error("exception", exception);
-
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }
 }
